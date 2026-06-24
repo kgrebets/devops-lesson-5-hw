39daae3a285bcc529ae80dec5621ccae70dbff3c
Kostiantyn <kog@yousee.dk>
2026-06-24 20:15:04 +0300

    ecr policy added and vpc add

--- modules/ecr/ecr.tf
+++ modules/ecr/ecr.tf
@@ -5,4 +5,38 @@ resource "aws_ecr_repository" "ecr_repository" {
   image_scanning_configuration {
     scan_on_push = var.scan_on_push
   }
+
+  encryption_configuration {
+    encryption_type = "AES256"
+  }
+}
+
+# Repository access policy
+resource "aws_ecr_repository_policy" "ecr_repository_policy" {
+  repository = aws_ecr_repository.ecr_repository.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+
+    Statement = [
+      {
+        Sid    = "AllowPushPull"
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::349348220835:root"
+        }
+
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload"
+        ]
+      }
+    ]
+  })
 }
