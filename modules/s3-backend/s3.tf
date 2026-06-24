9ce361d6c6b0102e5a9162ffa9c1ccc13d593af9
Kostiantyn <kog@yousee.dk>
2026-06-24 12:37:47 +0300

    add server-side encryption and block public acccess

--- modules/s3-backend/s3.tf
+++ modules/s3-backend/s3.tf
@@ -11,3 +11,24 @@ resource "aws_s3_bucket_versioning" "terraform_state_versioning" {
     status = "Enabled"
   }
 }
+
+
+// Enable server-side encryption
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state_encryption" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+// Block all public access
+resource "aws_s3_bucket_public_access_block" "terraform_state_public_access" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
